c1716d389ace6be44294ad15fa4af4a003553c1f
Shawn <ScoMorii@163.com>
2025-06-11 17:32:34 +0800

    fix

--- docs/.vuepress/.temp/blog/type.js
+++ docs/.vuepress/.temp/blog/type.js
@@ -1,4 +1,4 @@
-export const typesMap = JSON.parse("{\"article\":{\"/\":{\"path\":\"/article/\",\"indexes\":[7,8,18,19,20,21,22,23,9,2,1,0,16,17,3,4,5,6,14,15,24,25,10,11,12,13,26]}},\"star\":{\"/\":{\"path\":\"/star/\",\"indexes\":[3,7,14,8]}},\"timeline\":{\"/\":{\"path\":\"/timeline/\",\"indexes\":[18,19,20,21,22,23,9,7,2,1,0,16,17,3,4,5,6,14,15,8]}}}");
+export const typesMap = JSON.parse("{\"article\":{\"/\":{\"path\":\"/article/\",\"indexes\":[13,0,18,19,20,21,22,23,1,8,7,6,14,15,9,10,11,12,16,17,24,25,2,3,4,5,26]}},\"star\":{\"/\":{\"path\":\"/star/\",\"indexes\":[9,13,16,0]}},\"timeline\":{\"/\":{\"path\":\"/timeline/\",\"indexes\":[18,19,20,21,22,23,1,13,8,7,6,14,15,9,10,11,12,16,17,0]}}}");
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
